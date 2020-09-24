1502f31b861801ea0e835e946abb68dae5be339c
fishlips13 <fishlips13@gmail.com>
2020-09-24 07:19:40 +0100

    Moved names into a class, implemented dialogue

---
+++ NPCRenamer/NPCName.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NamedNPCs
+{
+    public class NPCName
+    {
+        public string Name { get; }
+        private List<string> dialogues;
+
+        private int levelSpawns = 0;
+        private int runSpawns = 0;
+        private string prevDialogue = "";
+
+        public NPCName(string name, List<string> dialogues)
+        {
+            Name = name;
+            this.dialogues = dialogues;
+        }
+
+        public void Use()
+        {
+            levelSpawns++;
+            runSpawns++;
+        }
+
+        public void ResetLevel()
+        {
+            levelSpawns = 0;
+        }
+
+        public void ResetRun()
+        {
+            runSpawns = 0;
+        }
+
+        public bool CanUse(int levelMax, int runMax)
+        {
+            return levelSpawns < levelMax && runSpawns < runMax;
+        }
+
+        public string GetRandomDialogue()
+        {
+            if (dialogues.Count == 0)
+                return "";
+
+
+            int index = UnityEngine.Random.Range(0, dialogues.Count);
+            if (prevDialogue != "")
+                dialogues.Add(prevDialogue);
+            prevDialogue = dialogues[index];
+            dialogues.RemoveAt(index);
+            
+            return prevDialogue;
+        }
+    }
+}
--- NPCRenamer/NamedNPCs.cs
+++ NPCRenamer/NamedNPCs.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Collections.Generic;
 using BepInEx;
 using HarmonyLib;
-using UnityEngine;
 using System.Reflection;
+using UnityEngine;
 
 namespace NamedNPCs
 {
@@ -25,9 +25,7 @@ namespace NamedNPCs
         public static string playerName = "";
         public static string priorityName = "";
 
-        public static List<string> names = new List<string>();
-        public static Dictionary<string, int> namesCountLevel = new Dictionary<string, int>();
-        public static Dictionary<string, int> namesCountRun = new Dictionary<string, int>();
+        public static Dictionary<string, NPCName> npcNamesDict = new Dictionary<string, NPCName>();
 
         public void Awake()
         {
@@ -41,9 +39,11 @@ namespace NamedNPCs
                     if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
                         continue;
 
-                    int equalsIndex = line.IndexOf('=');
-                    string paramName = line.Substring(0, equalsIndex).Trim();
-                    string paramValue = line.Substring(equalsIndex + 1, line.Length - equalsIndex - 1).Trim();
+                    string[] operands = line.Split('=');
+                    string paramName = operands[0].Trim();
+                    string paramValue = operands[1].Trim();
+
+                    paramValue = paramValue.Replace("\"", "");
 
                     if (bool.TryParse(paramValue, out bool paramBool)) // Booleans
                     {
@@ -75,8 +75,6 @@ namespace NamedNPCs
                     }
                     else // Strings
                     {
-                        paramValue = paramValue.Replace("\"", "");
-
                         switch (paramName)
                         {
                             case "PlayerName":
@@ -105,14 +103,15 @@ namespace NamedNPCs
                     if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
                         continue;
 
-                    string name = line.Trim();
+                    string[] entries = line.Trim().Split(',');
+                    string npcName = entries[0];
+                    List<string> dialogues = entries.Skip(1).ToList();
 
-                    if (name.ToLower() == priorityName.ToLower() || namesCountLevel.ContainsKey(name))
+                    if (npcName.ToLower() == priorityName.ToLower() || npcNamesDict.ContainsKey(npcName))
                         continue;
 
-                    names.Add(name);
-                    namesCountLevel.Add(name, 0);
-                    namesCountRun.Add(name, 0);
+                    //npcNamesList.Add(npcName);
+                    npcNamesDict.Add(npcName, new NPCName(npcName, dialogues));
                 }
             }
             catch
@@ -127,26 +126,29 @@ namespace NamedNPCs
             MethodInfo original = AccessTools.Method(typeof(Fader), "FadedIn");
             MethodInfo patch = AccessTools.Method(typeof(Renamer), "FadedIn_PrefixPatch");
             harmony.Patch(original, new HarmonyMethod(patch));
+
+            original = AccessTools.Method(typeof(Agent), "Say", new Type[] { typeof(string), typeof(bool) });
+            patch = AccessTools.Method(typeof(Renamer), "Say_PrefixPatch");
+            harmony.Patch(original, new HarmonyMethod(patch));
         }
 
         // FadedIn patch - replace all agent names
         // --- Triggered finally after level setup
         public static void FadedIn_PrefixPatch()
         {
-            Debug.Log("Agent Count: " + GameController.gameController.agentList.Count);
             if (!enableRenaming || GameController.gameController.levelEditing)
                 return;
 
             // Flush run name use on Floor 1-1
             if (GameController.gameController.sessionDataBig.curLevel == 1)
             {
-                foreach (string name in namesCountRun.Keys.ToList())
-                    namesCountRun[name] = 0;
+                foreach (NPCName npcName in npcNamesDict.Values)
+                    npcName.ResetRun();
             }
 
             // Flush level name use each level
-            foreach (string name in namesCountLevel.Keys.ToList())
-                namesCountLevel[name] = 0;
+            foreach (NPCName npcName in npcNamesDict.Values)
+                npcName.ResetLevel();
 
             // Find potential agents to name (heandle special cases)
             List<Agent> agentPool = new List<Agent>();
@@ -161,53 +163,52 @@ namespace NamedNPCs
             }
 
             // Find legal names
-            List<string> namePool = new List<string>();
-            foreach (string name in names)
+            List<NPCName> npcNamePool = new List<NPCName>();
+            foreach (NPCName npcName in npcNamesDict.Values)
             {
-                if (namesCountRun[name] < nameOccurancePerRunMax)
-                    namePool.Add(name);
+                if (npcName.CanUse(nameOccurancePerLevelMax, nameOccurancePerRunMax))
+                    npcNamePool.Add(npcName);
             }
 
             // Mass name indiscriminately in homebase
             if (GameController.gameController.sessionDataBig.curLevel == 0)
             {
-                List<string> closedNamePool = new List<string>();
+                List<NPCName> closedNamePool = new List<NPCName>();
                 List<Agent> closedAgentPool = new List<Agent>();
 
                 while (agentPool.Count > 0)
                 {
-                    int nameIndex = UnityEngine.Random.Range(0, namePool.Count);
+                    int nameIndex = UnityEngine.Random.Range(0, npcNamePool.Count);
 
-                    agentPool[0].agentRealName = namePool[nameIndex];
+                    agentPool[0].agentRealName = npcNamePool[nameIndex].Name;
 
-                    closedNamePool.Add(namePool[nameIndex]);
+                    closedNamePool.Add(npcNamePool[nameIndex]);
                     closedAgentPool.Add(agentPool[0]);
-                    namePool.RemoveAt(nameIndex);
+                    npcNamePool.RemoveAt(nameIndex);
                     agentPool.RemoveAt(0);
 
-                    if (namePool.Count == 0)
-                        namePool = closedNamePool;
+                    if (npcNamePool.Count == 0)
+                        npcNamePool = closedNamePool;
                 }
 
                 return;
             }
 
             // Narrow name selection by occurrence / availability
-            int replaceCount = Math.Min(namePool.Count, UnityEngine.Random.Range(levelSpawnsMin, levelSpawnsMax + 1));
-
-            List<string> nameCandidates = new List<string>();
+            int replaceCount = Math.Min(npcNamePool.Count, UnityEngine.Random.Range(levelSpawnsMin, levelSpawnsMax + 1));
+            
+            List<NPCName> nameCandidates = new List<NPCName>();
             for (int i = 0; i < replaceCount; i++)
             {
-                int index = UnityEngine.Random.Range(0, namePool.Count);
-                nameCandidates.Add(namePool[index]);
-                namePool.RemoveAt(index);
+                int index = UnityEngine.Random.Range(0, npcNamePool.Count);
+                nameCandidates.Add(npcNamePool[index]);
+                npcNamePool.RemoveAt(index);
             }
-
+            
             // Name agents
             while (nameCandidates.Count > 0 && agentPool.Count > 0)
             {
-                string name = nameCandidates[0];
-                if (namesCountLevel[name] == nameOccurancePerLevelMax || namesCountRun[name] == nameOccurancePerRunMax)
+                if (!nameCandidates[0].CanUse(nameOccurancePerLevelMax, nameOccurancePerRunMax))
                 {
                     nameCandidates.RemoveAt(0);
                     continue;
@@ -215,12 +216,18 @@ namespace NamedNPCs
 
                 int agentIndex = UnityEngine.Random.Range(0, agentPool.Count);
 
-                agentPool[agentIndex].agentRealName = name;
+                agentPool[agentIndex].agentRealName = nameCandidates[0].Name;
                 agentPool.RemoveAt(agentIndex);
 
-                namesCountLevel[name]++;
-                namesCountRun[name]++;
+                nameCandidates[0].Use();
             }
+        }
+
+        //Say patch - replace dialogue lines just-in-time
+        public static void Say_PrefixPatch(Agent __instance, ref string myMessage)
+        {
+            if (npcNamesDict.ContainsKey(__instance.agentRealName))
+                myMessage = npcNamesDict[__instance.agentRealName].GetRandomDialogue();
         }
     }
 }
